3a910808e2f507262f787b834835fc173c8c4c73
cl0ne <cl0ne@mithril.org.ua>
2016-12-18 22:18:43 +0200

    Implement simple echo server

--- main.go
+++ main.go
@@ -1,5 +1,33 @@
 package main
 
+import (
+	"io"
+	"log"
+	"net"
+)
+
+func handleConnection(c net.Conn) {
+	log.Println("Accepted connection from:", c.RemoteAddr())
+	io.Copy(c, c)
+	log.Println("Client served:", c.RemoteAddr())
+	c.Close()
+}
+
 func main() {
-	println("hello!")
+	listenAddress := ":1337"
+	l, err := net.Listen("tcp", listenAddress)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer l.Close()
+
+	log.Println("Serving on", l.Addr())
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+		go handleConnection(conn)
+	}
 }
